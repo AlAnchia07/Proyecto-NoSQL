ea016d5478b50e9b9f26f0bf7cc96c5ecdb8e269
AlAnchia07 <janchia80563@ufide.ac.cr>
2026-07-18 19:32:26 -0600

    Estructura Basica del Proyecto
    FrontEnd con Vue y Backend con Node.js

---
+++ BackEnd/src/config/db.js
@@ -0,0 +1,16 @@
+// src/config/db.js
+const mongoose = require('mongoose');
+ 
+ 
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://admin:password_123@localhost:27017/Simulacion2?authSource=admin', { //Aqui hay que cambiar la ruta
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+ 
+module.exports = connectDB;
