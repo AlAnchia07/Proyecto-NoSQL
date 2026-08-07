ca2038282ea0f6e5c838b9f32b311767f235ba92
AlAnchia07 <janchia80563@ufide.ac.cr>
2026-08-07 14:04:22 -0600

    Cambios esteticos

--- FrontEnd/src/components/reviews/ReviewForm.vue
+++ FrontEnd/src/components/reviews/ReviewForm.vue
@@ -50,8 +50,7 @@
 
 <template>
         <button @click="mostrarModal = true" type="button" class="btn btn-success" style="width: 12rem;">
-            <i class="bi bi-pen-fill"></i>
-            Nueva reseña
+            <i class="bi bi-pen-fill"></i>  <strong>Nueva reseña</strong>
         </button>
 
     <ModalReseña
